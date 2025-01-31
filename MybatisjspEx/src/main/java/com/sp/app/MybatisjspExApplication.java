36dfcdaccc7869d78f686ae2c2060c9ed57d13ce
user <user@DESKTOP-H3PVLIA>
2025-01-31 16:18:14 +0900

    realpumpkin

---
+++ MybatisjspEx/src/main/java/com/sp/app/MybatisjspExApplication.java
@@ -0,0 +1,13 @@
+package com.sp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisjspExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisjspExApplication.class, args);
+	}
+
+}
